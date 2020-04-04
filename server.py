449153e89a94116168e5c90608c01cde4d6a5d1a
ExAxee <dmr475@gmail.com>
2020-04-04 16:16:19 +0200

    Reviewed server.py code, added closeConnection and stopServer functions to server.py

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ global HEADER
 HEADER = 4
 
 class Server:
-    self.__triggers = []
     self.connections = []
     
     def __init__(self,
@@ -13,39 +12,36 @@ class Server:
         if reuse_addr:
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    def startserver(self, IP: str, trigger_conn: callable, trigger_data: callable, buffer=5: int, port=7530: int):
+    def startServer(self,
+                    IP: str,
+                    port=7530: int,
+                    data_request_trigger: callable,
+                    connection_request_trigger=None: callable,
+                    buffer=5: int):
+        
         self.socket.bind( (IP, port) )
         self.socket.listen(buffer)
     
         while True:
             connection, address = self.socket.accept()
-            trigger_conn(connection, address)
-            data, address = self.socket.recvfrom(HEADER)
-            data, address = 
-    
-    """
-    def on_connection_request(self, func):
-        connection, address = self.socket.accept()
-        func(connection, address)
-    
-    def on_recieve(self, func, mode=1: int):
-        """mode 1 = recieve bytes, mode 2 = recieve bytes and address"""
-        data, address = self.socket.recvfrom(10)
-        
-    
-    def add_handler(self, func: callable, trigger: str):
-        pass
-        
-        while 1:
-            clientsocket, address = self.socket.accept()
-            print(f"Connection with {address} established.")
+            self.connections.append( (connection, address) )
+            if connection_request_trigger != None:
+                connection_request_trigger(connection, address)
             
-            self.msg = "Welcome to the server!"
-            self.msg = f"{len(self.msg):<{HEADER}}" + self.msg
-            clientsocket.send(bytes(self.msg,"utf-8"))
-            
-            while 1:
-                self.msg = str(input("string to send >>> "))
-                self.msg = f"{len(self.msg):<{HEADER}}" + self.msg
-                clientsocket.send(bytes(self.msg,"utf-8"))
-    """
+            head, address = self.socket.recvfrom(HEADER)
+            data, address = self.socket.recvfrom(int(head))
+            data_request_trigger(data, address)
+    
+    def closeConnection(connection, msg = None: str):
+        """closes a connection from a client, if msg is specified the server will send that msg and after will close the connection"""
+        if msg:
+            connection.send(bytes(
+                f"{len(msg):<{HEADER}}" + msg
+                ))
+        connection.shutdown(socket.SHUT_RDWR)
+        connection.close()
+    
+    def stopServer():
+        """Shuts down the server"""
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
