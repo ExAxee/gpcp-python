91f935ce7b09180cee15bb34cee89222504212ec
Stypox <stypox@pm.me>
2020-04-07 21:15:18 +0200

    Remove trailing whitespaces

--- client.py
+++ client.py
@@ -2,50 +2,50 @@ import socket
 
 class Client:
     HEADER = 4
-    
+
     def __init__(self, default_handler=None):
         """if default_handler is specified all responses to all requests will be handled by this callable"""
-        
+
         if default_handler:
             if not isinstance(default_handler, callable):
                 raise ValueError(f"invalid option '{default_handler}' for default_handler, must be callable")
-        
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     def connect(self, host, port):
         """connect to a server"""
-        
+
         if not isinstance(host, str):
                 raise ValueError(f"invalid option '{host}' for host, must be string")
         if not isinstance(port, int):
                 raise ValueError(f"invalid option '{port}' for port, must be integer")
-        
+
         self.socket.connect( (host, port) )
-    
+
     def request(self, request, handler=None):
         """if handler is specified it will overwrite temporairly the default_handler"""
-        
+
         if not isinstance(default_handler, callable):
                 raise ValueError(f"invalid option '{handler}' for handler, must be callable")
-        
-        if isinstance(request, bytes): 
+
+        if isinstance(request, bytes):
             self.socket.sendall(bytes(f"{len(request):<{HEADER}}") + request)
         else:
             self.socket.sendall(bytes(f"{len(request):<{HEADER}}" + request))
-        
+
         head = self.socket.recv(HEADER) #read the header from a buffered request
         data = self.socket.recv(int(head)) #read the actual message of len head
-        
+
         while len(data) < head:
             data += self.socket.recv( int(head) - len(data) )
-        
+
         if handler != None:
             handler(self.socket, data)
         elif default_handler != None:
             default_handler(self.socket, data)
         else:
             return data
-    
+
     def closeConnection(self, mode = "RW"):
         """closes the connection to the server, RW = read and write, R = read, W = write"""
         if mode == "RW":
@@ -56,6 +56,6 @@ class Client:
             self.socket.shutdown(socket.SHUT_WR)
         else:
             raise ValueError("close mode must be 'R' or 'W' or 'RW'")
-        
+
         self.socket.close()
 
