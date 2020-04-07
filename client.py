6b5c1689ba5ee452db3d01b133d027ea335319ce
ExAxee <dmr475@gmail.com>
2020-04-07 19:16:31 +0200

    fixed check for callable argument types in client.py and server.py

--- client.py
+++ client.py
@@ -3,15 +3,30 @@ import socket
 class Client:
     HEADER = 4
     
-    def __init__(self, default_handler=None: callable):
+    def __init__(self, default_handler=None):
         """if default_handler is specified all responses to all requests will be handled by this callable"""
+        
+        if default_handler:
+            if not isinstance(default_handler, callable):
+                raise ValueError(f"invalid option '{default_handler}' for default_handler, must be callable")
+        
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    def connect(self, host: str, port: int):
+    def connect(self, host, port):
+        """connect to a server"""
+        
+        if not isinstance(host, str):
+                raise ValueError(f"invalid option '{host}' for host, must be string")
+        if not isinstance(port, int):
+                raise ValueError(f"invalid option '{port}' for port, must be integer")
+        
         self.socket.connect( (host, port) )
     
-    def request(self, request, handler=None: callable):
+    def request(self, request, handler=None):
         """if handler is specified it will overwrite temporairly the default_handler"""
+        
+        if not isinstance(default_handler, callable):
+                raise ValueError(f"invalid option '{handler}' for handler, must be callable")
         
         if isinstance(request, bytes): 
             self.socket.sendall(bytes(f"{len(request):<{HEADER}}") + request)
@@ -31,7 +46,7 @@ class Client:
         else:
             return data
     
-    def closeConnection(self, mode = "RW": str):
+    def closeConnection(self, mode = "RW"):
         """closes the connection to the server, RW = read and write, R = read, W = write"""
         if mode == "RW":
             self.socket.shutdown(socket.SHUT_RDWR)
