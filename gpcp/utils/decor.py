a5726b7158a89ed541f26126ce19c73dd59f7fa4
Stypox <stypox@pm.me>
2020-04-14 14:31:56 +0200

    Improve code with pylint and add comments

--- gpcp/utils/decor.py
+++ gpcp/utils/decor.py
@@ -2,25 +2,31 @@ from .base_handler import BaseHandler
 from .base_types import TypeBase, String
 
 def command(arg):
-	def getArgumentTypes(func):
-		typedArguments = func.__annotations__
+    def getArgumentTypes(func):
+        typedArguments = func.__annotations__
 
-		argumentTypes = []
-		for argName in func.__code__.co_varnames[2:]:
-			argumentType = typedArguments.get(argName, String)
-			argumentTypes.append(TypeBase.getIfBuiltIn(argumentType))
+        argumentTypes = []
+        for argName in func.__code__.co_varnames[2:]:
+            # for every argument of the decorated function the type is obtained (if specified with
+            # `argument: type`) or defaulted to `String`. Built-in types are supported.
+            argumentType = typedArguments.get(argName, String)
+            argumentTypes.append(TypeBase.getIfBuiltIn(argumentType))
 
-		return argumentTypes
+        return argumentTypes
 
-	if callable(arg):
-		arg.__gpcp_metadata__ = (BaseHandler.FunctionType.command, arg.__name__, getArgumentTypes(arg))
-		return arg
-	else:
-		def wrapper(func):
-			func.__gpcp_metadata__ = (BaseHandler.FunctionType.command, arg, getArgumentTypes(func))
-			return func
-		return wrapper
+    if callable(arg):
+        # `@command` used without parameters
+        arg.__gpcp_metadata__ = (BaseHandler.FunctionType.command,
+                                 arg.__name__, getArgumentTypes(arg))
+        return arg
+
+    # `@command` used with name parameter (e.g. @command("start"))
+    def wrapper(func):
+        func.__gpcp_metadata__ = (BaseHandler.FunctionType.command,
+                                  arg, getArgumentTypes(func))
+        return func
+    return wrapper # the returned function when called adds the metadata to `func` and returns it
 
 def unknownCommand(func):
-	func.__gpcp_metadata__ = (BaseHandler.FunctionType.unknown,)
-	return func
+    func.__gpcp_metadata__ = (BaseHandler.FunctionType.unknown,)
+    return func
